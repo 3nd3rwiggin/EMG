4ee7bdf5c500e603cfd549ee6cfa0f1a229b9199
3nd3rwiggin <105644578+3nd3rwiggin@users.noreply.github.com>
2023-06-10 13:17:47 +0530

    emg_sim.py

--- emg_sim.py
+++ emg_sim.py
@@ -6,19 +6,21 @@ from scipy.io import savemat
 
 class EMGSimulator:
     def __init__(self, force):
-        self.force_slope = force/10
+        
         self.duration = 10
+        self.force_slope = force/self.duration
         self.sampling_rate = 2000
         self.time = np.arange(0, self.duration, 1/self.sampling_rate)
         self.force_profile = self.force_slope * self.time
         self.force_value = self.force_slope * self.duration
         self.force_thresholds = {1: [0,20], 2: [20,40], 3: [40,60]}
-        self.max_firing_rates = {1: 3, 2: 6, 3: 8}
+        self.max_firing_rates = {1: 3, 2: 6, 3: 9}
         self.muap_params = {
-            1: [0.2, 0.2, 0.1, 0.15, 0.02],
-            2: [0.6, 0.6, 0.08, 0.12, 0.03],
-            3: [0.8, 0.8, 0.06, 0.09, 0.04]
-        }
+    1: [0.2, 0.2, 0.1, 0.15, 0.02],
+    2: [0.6, 0.6, 0.08, 0.12, 0.03],
+    3: [1.4, 1.4, 0.1, 0.15, 0.02]
+}
+
 
     @staticmethod
     def generate_biphasic_muap(t, muap_params, force_level):
@@ -83,48 +85,48 @@ class EMGSimulator:
         savemat("emg.mat", data_to_export)
 
     def plot_all(self, firing_rates, spike_trains, emg, muaps):
-        fig, axs = plt.subplots(5, 1, figsize=(10, 25))
-        fig.subplots_adjust(hspace=0.5)
+      fig, axs = plt.subplots(5, 1, figsize=(10, 30))
+      fig.subplots_adjust(hspace=0.5)
 
-        # Plot force profile
-        axs[0].plot(self.time, self.force_profile, label=f"Force {self.force_value}")
-        axs[0].set(xlabel="Time (s)", ylabel="Force (N)")
-        axs[0].legend()
-        axs[0].set_title("Force Profile")
-        axs[0].grid(True)
+      # Plot force profile
+      axs[0].plot(self.time, self.force_profile, label=f"Force {self.force_value}")
+      axs[0].set(xlabel="Time (s)", ylabel="Force (N)")
+      axs[0].legend()
+      axs[0].set_title("Force Profile")
+      axs[0].grid(True)
 
-        # Plot firing rates
-        for motor_unit, firing_rate in firing_rates.items():
-            axs[1].plot(self.time, firing_rate, label=f"Motor Unit {motor_unit}")
-        axs[1].set(xlabel="Time (s)", ylabel="Firing Rate (Hz)")
-        axs[1].legend()
-        axs[1].set_title("Firing Rate")
-        axs[1].grid(True)
+      # Plot firing rates
+      for motor_unit, firing_rate in firing_rates.items():
+          axs[1].plot(self.time, firing_rate, label=f"Motor Unit {motor_unit}")
+      axs[1].set(xlabel="Time (s)", ylabel="Firing Rate (Hz)")
+      axs[1].legend()
+      axs[1].set_title("Firing Rate")
+      axs[1].grid(True)
 
-        # Plot spike trains
-        for motor_unit, spike_train in spike_trains.items():
-            axs[2].eventplot(np.where(spike_train)[0] / self.sampling_rate, lineoffsets=motor_unit, label=f"Motor Unit {motor_unit}")
-        axs[2].set(xlabel="Time (s)", ylabel="Motor Unit")
-        axs[2].set_yticks(list(spike_trains.keys()))
-        axs[2].legend()
-        axs[2].set_title("Spike Train")
-        axs[2].grid(True)
+      # Plot spike trains
+      for motor_unit, spike_train in spike_trains.items():
+          axs[2].eventplot(np.where(spike_train)[0] / self.sampling_rate, lineoffsets=motor_unit, label=f"Motor Unit {motor_unit}")
+      axs[2].set(xlabel="Time (s)", ylabel="Motor Unit")
+      axs[2].set_yticks(list(spike_trains.keys()))
+      axs[2].legend()
+      axs[2].set_title("Spike Train")
+      axs[2].grid(True)
 
-        # Plot motor unit action potentials
-        for motor_unit, muap in muaps.items():
-            axs[3].plot(self.time, muap, label=f"Motor Unit {motor_unit}")
-        axs[3].set(xlabel="Time (s)", ylabel="Potential")
-        axs[3].legend()
-        axs[3].set_title("Motor Unit Action Potential")
-        axs[3].grid(True)
+      # Plot motor unit action potentials
+      for motor_unit, muap in muaps.items():
+          axs[3].plot(self.time, muap, label=f"Motor Unit {motor_unit}")
+      axs[3].set(xlabel="Time (s)", ylabel="Potential")
+      axs[3].legend()
+      axs[3].set_title("Motor Unit Action Potential")
+      axs[3].grid(True)
 
-        # Plot EMG signal
-        axs[4].plot(self.time, emg, label="EMG Signal")
-        axs[4].set(xlabel="Time (s)", ylabel="EMG Signal")
-        axs[4].set_title("EMG Signal")
-        axs[4].grid(True)
+      # Plot EMG signal
+      axs[4].plot(self.time, emg, label="EMG Signal")
+      axs[4].set(xlabel="Time (s)", ylabel="EMG Signal")
+      axs[4].set_title("EMG Signal")
+      axs[4].grid(True)
 
-        plt.show()
+      plt.show()
 
     def run_simulation(self):
         firing_rates = self.calculate_firing_rates()
@@ -132,7 +134,11 @@ class EMGSimulator:
         emg, muaps = self.generate_emg(spike_trains)
         self.export_to_mat(emg)
         self.plot_all(firing_rates, spike_trains, emg, muaps)
+        
 
+    
+      
+      
 
 if __name__ == "__main__":
     simulator = EMGSimulator(force=55)
